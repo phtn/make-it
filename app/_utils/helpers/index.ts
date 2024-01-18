b471772d5846965a8b6d02e2efc65be278a1357f
Jun Orville Lecena <jun.lecena@comptrolla.com>
2024-01-19 03:48:58 +0800

    fix: args arr

--- app/_utils/helpers/index.ts
+++ app/_utils/helpers/index.ts
@@ -22,25 +22,25 @@ export function getNextElement<T>(
   return nextIndex;
 }
 
-export const onError = (args: string[]) => {
+export const onError = (...args: string[]) => {
   toast.error(args[0], {
     description: args[1],
   });
 };
 
-export const onSuccess = (args: string[]) => {
+export const onSuccess = (...args: string[]) => {
   toast.success(args[0], {
     description: args[1],
   });
 };
 
-export const onInfo = (args: string[]) => {
+export const onInfo = (...args: string[]) => {
   toast.info(args[0], {
     description: args[1],
   });
 };
 
-export const onWarn = (args: string[]) => {
+export const onWarn = (...args: string[]) => {
   toast.warning(args[0], {
     description: args[1],
   });
